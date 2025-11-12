c69f8bb3b9f52f89ae1397e88d51b36fbc52bfdd
diegouidev <diegocoelho44@gmail.com>
2025-11-12 13:51:48 -0300

    ajustes na urls

--- api-grafica/core/urls.py
+++ api-grafica/core/urls.py
@@ -3,6 +3,10 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .views import (
     ClienteViewSet, ProdutoViewSet, OrcamentoViewSet, ItemOrcamentoViewSet,
     PedidoViewSet, ItemPedidoViewSet, DashboardStatsView, PagamentoViewSet, 
@@ -50,6 +54,9 @@ router.register(r'admin/users', UserManagementViewSet, basename='admin-user')
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('public/empresa/', EmpresaPublicaView.as_view(), name='empresa-publica'),
     
     path(
