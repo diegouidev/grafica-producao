fb308d89be1a497319096ef90999ad033b466bb3
Diego Coelho Lins <105721945+diegouidev@users.noreply.github.com>
2025-11-12 16:32:42 -0300

    Implement NotificacaoAdmin for admin interface
    Added NotificacaoAdmin class to manage Notificacao model in admin.

--- api-grafica/notificacoes/admin.py
+++ api-grafica/notificacoes/admin.py
@@ -1,3 +1,22 @@
+# notificacoes/admin.py
 from django.contrib import admin
+from .models import Notificacao
 
-# Register your models here.
+@admin.register(Notificacao)
+class NotificacaoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'mensagem_curta', 'lida', 'data_criacao', 'link')
+    list_filter = ('lida', 'data_criacao', 'usuario')
+    search_fields = ('usuario__username', 'mensagem')
+    list_editable = ('lida',)
+    list_per_page = 30
+    
+    # Define campos que não devem ser editáveis (são gerados pelo sistema)
+    readonly_fields = ('usuario', 'mensagem', 'link', 'data_criacao', 'chave_unica')
+
+    @admin.display(description='Mensagem')
+    def mensagem_curta(self, obj):
+        return f"{obj.mensagem[:75]}..." if len(obj.mensagem) > 75 else obj.mensagem
+
+    # Impede que administradores criem notificações manualmente
+    def has_add_permission(self, request):
+        return False
